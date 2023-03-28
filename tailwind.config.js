0e6514dd880d1f259a8221cf135392f5ff47fa97
FizzyOne13 <119670800+FizzyOne13@users.noreply.github.com>
2023-03-28 16:07:05 +0300

    cv-page project layout base beta

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.js", "./public/**/*.html"],
+  theme: {
+    theme: {
+      colors: {
+        blue: "#1fb6ff",
+        purple: "#7e5bef",
+        pink: "#ff49db",
+        orange: "#ff7849",
+        green: "#13ce66",
+        yellow: "#ffc82c",
+        "gray-dark": "#273444",
+        gray: "#8492a6",
+        "gray-light": "#d3dce6",
+        height: {
+          "10v": "10vh",
+          "20v": "20vh",
+          "30v": "30vh",
+          "40v": "40vh",
+          "50v": "50vh",
+          "60v": "60vh",
+          "70v": "70vh",
+          "80v": "80vh",
+          "90v": "90vh",
+          "100v": "100vh",
+        },
+        widht: {
+          "10v": "10vh",
+          "20v": "20vh",
+          "30v": "30vh",
+          "40v": "40vh",
+          "50v": "50vh",
+          "60v": "60vh",
+          "70v": "70vh",
+          "80v": "80vh",
+          "90v": "90vh",
+          "100v": "100vh",
+        },
+      },
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+      extend: {
+        spacing: {},
+        borderRadius: {},
+      },
+    },
+  },
+  plugins: [],
+};
