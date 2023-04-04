1e15b57cc8aae2ca177173c05179c6c22865a53b
FizzyOne13 <119670800+FizzyOne13@users.noreply.github.com>
2023-04-05 02:21:55 +0300

    responsive struggle
    need to do some script with data gathering and putting desired name of user through form or modal
    need to overcome responsive at last
    light & dark themes
    nav mb

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: ["./public/*.js", "./public/**/*.html"],
   theme: {
+    theme: {
       colors: {
         blue: "#1fb6ff",
         purple: "#7e5bef",
@@ -13,43 +14,44 @@ module.exports = {
         gray: "#8492a6",
         "gray-light": "#d3dce6",
       },
-        height: {
-          "10v": "10vh",
-          "20v": "20vh",
-          "30v": "30vh",
-          "40v": "40vh",
-          "50v": "50vh",
-          "60v": "60vh",
-          "70v": "70vh",
-          "80v": "80vh",
-          "90v": "90vh",
-          "100v": "100vh",
-        },
-        widht: {
-          "10v": "10vh",
-          "20v": "20vh",
-          "30v": "30vh",
-          "40v": "40vh",
-          "50v": "50vh",
-          "60v": "60vh",
-          "70v": "70vh",
-          "80v": "80vh",
-          "90v": "90vh",
-          "100v": "100vh",
-        },
+      height: {
+        "10v": "10vh",
+        "20v": "20vh",
+        "30v": "30vh",
+        "40v": "40vh",
+        "50v": "50vh",
+        "60v": "60vh",
+        "70v": "70vh",
+        "80v": "80vh",
+        "90v": "90vh",
+        "100v": "100vh",
       },
-      fontFamily: {
-        sans: ["Graphik", "sans-serif"],
-        serif: ["Merriweather", "serif"],
-        josefin: ["Josefin Sans", "sans-serif"],
+      widht: {
+        "10v": "10vh",
+        "20v": "20vh",
+        "30v": "30vh",
+        "40v": "40vh",
+        "50v": "50vh",
+        "60v": "60vh",
+        "70v": "70vh",
+        "80v": "80vh",
+        "90v": "90vh",
+        "100v": "100vh",
       },
-      extend: {
-        fontFamily: {},
-        spacing: {},
-        borderRadius: {},
-        backgroundImage: {
-          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        },
     },
-    plugins: [],
-  };
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      josefin: ["Josefin Sans", "sans-serif"],
+    },
+  },
+  extend: {
+    fontFamily: {},
+    spacing: {},
+    borderRadius: {},
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+    },
+  },
+  plugins: [],
+};
